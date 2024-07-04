678b8b5bb0b0dd623318a68bddf7df9c339a959f
sterino <egore.chip@gmail.com>
2024-07-04 23:13:54 +0500

    Fix proxy.go

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"app/internal/router"
+	"fmt"
+	"net/http"
 )
 
 func main() {
+
 	r := router.Router()
 
+	if err := http.ListenAndServe(":8080", r); err != nil {
+
+		fmt.Println("Error starting server:", err)
+	}
 }
