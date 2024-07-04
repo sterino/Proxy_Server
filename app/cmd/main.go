065b5452a8ac087a17d6d6f3f2d2094b99fb6eb7
sterino <egore.chip@gmail.com>
2024-07-04 19:58:55 +0500

    Your commit message here

---
+++ app/cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"app/internal/router"
+)
+
+func main() {
+	r := router.Router()
+
+}
---
+++ app/internal/router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Router() http.Handler {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	return router
+}
